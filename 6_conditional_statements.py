e3ee63e4d3a96d971b42e18bd2bba8c9c63f83bf
jmbharathram <41310420+jmbharathram@users.noreply.github.com>
2020-06-20 11:05:40 -0700

    Create 6_conditional_statements.py

---
+++ 6_conditional_statements.py
@@ -0,0 +1,9 @@
+age = 10
+if age < 13:
+   print("You're a child")
+elif age >= 13 and age < 18:
+   print("You're a teenager.")
+elif age >= 18:
+   print("You're an Adult.")
+else:
+   print("Huh???")
