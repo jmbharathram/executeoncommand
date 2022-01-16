93bd5f6334da845461af31d345024bcd42cbf5f6
jmbharathram <41310420+jmbharathram@users.noreply.github.com>
2022-01-15 16:22:59 -0800

    Create mysql-program.py

---
+++ mysql-program.py
@@ -0,0 +1,23 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    connection = mysql.connector.connect(host='service ip',
+                                         database='mysql',
+                                         user='root',
+                                         password='abc')
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
+
+except Error as e:
+    print("Error while connecting to MySQL", e)
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed")
